6ebb91c65d97bca6568304e7d34e50bc45e79418
SManotsi <142833379+SManotsi@users.noreply.github.com>
2025-01-23 20:08:32 +0200

    Add files via upload

---
+++ Models/ApplicationDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.IO;
+
+namespace FileManagementSystem.Models
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+
+        public DbSet<FileModel> Files { get; set; }
+    }
+}
