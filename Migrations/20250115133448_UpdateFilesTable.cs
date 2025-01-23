46ebfb203ce6b936f26851a0b24ed801eeff8616
SManotsi <142833379+SManotsi@users.noreply.github.com>
2025-01-23 20:06:31 +0200

    Add files via upload

---
+++ Migrations/20250115133448_UpdateFilesTable.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FileManagementSystem.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateFilesTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
